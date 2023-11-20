1f131d458d9936cf25d444dc37ca16714f2b41c9
Lulus Bagos Hermawan <42728707+lulusbagos@users.noreply.github.com>
2023-11-20 08:42:11 +0700

    Update db.js
    delete DB

--- helpers/db.js
+++ helpers/db.js
@@ -16,24 +16,6 @@ const createConnection = async() => {
 return await pool.connect();
 };
 
-const getReply = async (keyboard, id_group) => {
-if (keyboard.includes('/cek') ||  groups.includes('!groups')){
-try {
-const pool = await createConnection();
-const request = pool.request();
-request.input('keyboard', sql.VarChar(50), keyboard); // declare the 'keyboard' parameter
-const result = await request.query('SELECT isi as message FROM [dbo].[TG_FN_PATRICIA_COMMAND_WA](@keyboard)');
-if (result.recordset.length > 0) return result.recordset[0].message;
-return false;
-} catch (err) {
-console.error('Error executing query:', err);
-return false;
-} finally {
-if (pool) pool.close();
-}
-}
-};
-
 const getMessage = () => {
 setInterval(getMessagesToSend, 3000); // send message every 5 minutes (300000 milliseconds)
 };
